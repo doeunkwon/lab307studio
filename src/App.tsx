1a4316bc0dc0f71a2c183256202fc692449d2801
Doeun Kwon <dekwooon@gmail.com>
2025-04-25 23:21:46 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>LAB307 Technologies Inc.</h2>
+      <h2 style={{ fontWeight: "100" }}>
+        We build apps that solve big problems in simple ways.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
