538ce56c33a794fd973162532afc1fc9cf1bc418
Doeun Kwon <dekwooon@gmail.com>
2025-05-21 22:10:31 -0700

    add support link

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>LAB307 Technologies Inc.</h2>
-      <h2 style={{ fontWeight: "100" }}>
-        We build apps that solve big problems in simple ways.
-      </h2>
+      <div>LAB307 Technologies Inc.</div>
+      <div>We develop and distribute apps.</div>
+      <div>
+        <a href="mailto:support@lab307.studio">support@lab307.studio</a>
+      </div>
     </div>
   );
 }
