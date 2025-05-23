d3c63a0e7f705a8b246b92cdcafee955d4a7e732
Doeun Kwon <dekwooon@gmail.com>
2025-05-23 15:13:47 -0700

    fix styling on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.NODE_ENV === 'production';
-
-const repo = 'lab307studio';
-
-module.exports = {
-  output: 'export',
-  basePath: isGithubPages ? `/${repo}` : '',
-  assetPrefix: isGithubPages ? `/${repo}/` : '',
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
